33635512ed34fa21a58c7e97d2222d70793bfa2a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-15 22:30:08 +0700

    add PlaceCommandNotFoundException class

---
+++ src/main/java/nawaphon/play/exceptions/PlaceCommandNotFoundException.java
@@ -0,0 +1,8 @@
+package nawaphon.play.exceptions;
+
+
+public class PlaceCommandNotFoundException extends RuntimeException {
+    public PlaceCommandNotFoundException(String message) {
+        super(message);
+    }
+}
