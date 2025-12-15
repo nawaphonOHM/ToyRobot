ed8392e7e1de6993f3961684124082df53fa7361
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-15 22:30:21 +0700

    update PlaceCommandNotFoundException with default message

--- src/main/java/nawaphon/play/exceptions/PlaceCommandNotFoundException.java
+++ src/main/java/nawaphon/play/exceptions/PlaceCommandNotFoundException.java
@@ -2,7 +2,7 @@ package nawaphon.play.exceptions;
 
 
 public class PlaceCommandNotFoundException extends RuntimeException {
-    public PlaceCommandNotFoundException(String message) {
-        super(message);
+    public PlaceCommandNotFoundException() {
+        super("Place command not found.");
     }
 }
