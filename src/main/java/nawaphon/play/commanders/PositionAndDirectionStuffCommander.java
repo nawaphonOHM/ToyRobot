c5f437a44b568ef2fbd39f28e6d07aed043f25c2
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-15 23:41:06 +0700

    Inject `DirectionNode` dependency into `PositionAndDirectionStuffCommander`

--- src/main/java/nawaphon/play/commanders/PositionAndDirectionStuffCommander.java
+++ src/main/java/nawaphon/play/commanders/PositionAndDirectionStuffCommander.java
@@ -1,11 +1,14 @@
 package nawaphon.play.commanders;
 
 
+import nawaphon.play.pojos.DirectionNode;
 import nawaphon.play.pojos.Position;
 
 public class PositionAndDirectionStuffCommander {
 
     private final Position positionModule;
+
+    private DirectionNode currentDirection;
 
     public PositionAndDirectionStuffCommander(Position positionModule) {
         this.positionModule = positionModule;
