f37ba297fb85f4305895e533c34102ca65cf8796
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-15 22:56:41 +0700

    update InitializePositionIncorrectException with a default message detailing expected position range

--- src/main/java/nawaphon/play/exceptions/InitializePositionIncorrectException.java
+++ src/main/java/nawaphon/play/exceptions/InitializePositionIncorrectException.java
@@ -2,7 +2,14 @@ package nawaphon.play.exceptions;
 
 
 public class InitializePositionIncorrectException extends RuntimeException {
-    public InitializePositionIncorrectException(String message) {
-        super(message);
+    public InitializePositionIncorrectException() {
+        super("""
+                Expected Position:
+                
+                0 <= x <= 5
+                0 <= y <= 5
+                
+       
+                """);
     }
 }
