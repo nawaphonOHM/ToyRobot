60434985a87b27e8b12f0051ff70616f6ee2ff19
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-15 23:06:50 +0700

    add makeDirectionGraphAndReturnInitialDirection method to UntilCommander class

--- src/main/java/nawaphon/play/commanders/UntilCommander.java
+++ src/main/java/nawaphon/play/commanders/UntilCommander.java
@@ -1,5 +1,12 @@
 package nawaphon.play.commanders;
 
 
+import nawaphon.play.enums.Direction;
+import nawaphon.play.pojos.DirectionNode;
+
 public abstract class UntilCommander {
+
+    public static DirectionNode makeDirectionGraphAndReturnInitialDirection(Direction initialDirection) {
+        return null;
+    }
 }
