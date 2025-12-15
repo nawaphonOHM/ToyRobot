9a51dcbb82e80f94f60873903231b41f0d3a9ebe
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-15 23:41:58 +0700

    Add `computeMoveXAndMoveY` method to handle movement logic based on direction

--- src/main/java/nawaphon/play/commanders/PositionAndDirectionStuffCommander.java
+++ src/main/java/nawaphon/play/commanders/PositionAndDirectionStuffCommander.java
@@ -1,8 +1,11 @@
 package nawaphon.play.commanders;
 
 
+import nawaphon.play.enums.Direction;
 import nawaphon.play.pojos.DirectionNode;
 import nawaphon.play.pojos.Position;
+import org.jetbrains.annotations.Contract;
+import org.jspecify.annotations.NonNull;
 
 public class PositionAndDirectionStuffCommander {
 
@@ -15,5 +18,29 @@ public class PositionAndDirectionStuffCommander {
 
     public PositionAndDirectionStuffCommander(Position positionModule) {
         this.positionModule = positionModule;
+    }
+
+    @Contract(mutates = "this")
+    private void computeMoveXAndMoveY (@NonNull Direction direction) {
+
+        switch (direction) {
+            case NORTH -> {
+                moveYDirection = 1;
+                moveXDirection = 0;
+            }
+            case WEST -> {
+                moveYDirection = 0;
+                moveXDirection = -1;
+            }
+            case SOUTH -> {
+                moveYDirection = -1;
+                moveXDirection = 0;
+            }
+            case EAST -> {
+                moveYDirection = 0;
+                moveXDirection = 1;
+            }
+        }
+
     }
 }
