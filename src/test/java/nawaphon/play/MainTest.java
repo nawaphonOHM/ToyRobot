e0548d6511e67f5bd7e861a7479f7df69421bb5c
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-16 00:29:44 +0700

    Add initial test structure with `MainTest` class

---
+++ src/test/java/nawaphon/play/MainTest.java
@@ -0,0 +1,12 @@
+package nawaphon.play;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
