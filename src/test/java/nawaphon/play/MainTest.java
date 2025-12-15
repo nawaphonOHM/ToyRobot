5000e0b6b1bb6815eda4da27c3fdbfc51bedb05e
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-16 00:29:58 +0700

    Refactor `MainTest` to add `originalSystemIn` field for future input stream handling

--- src/test/java/nawaphon/play/MainTest.java
+++ src/test/java/nawaphon/play/MainTest.java
@@ -2,9 +2,13 @@ package nawaphon.play;
 
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    private final InputStream originalSystemIn = System.in;
 
     @BeforeEach
     void setUp() {
