71b97b8744c6f4092d74d89dcdb658b96bec6462
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-15 22:06:49 +0700

    completed DirectionNode class with direction, left, and right properties

--- src/main/java/nawaphon/play/pojos/DirectionNode.java
+++ src/main/java/nawaphon/play/pojos/DirectionNode.java
@@ -1,4 +1,38 @@
 package nawaphon.play.pojos;
 
 
-public class DirectionNode {}
+import nawaphon.play.enums.Direction;
+
+public class DirectionNode {
+
+    private final Direction direction;
+
+    private DirectionNode left;
+
+    private DirectionNode right;
+
+
+    public DirectionNode(Direction direction) {
+        this.direction = direction;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public DirectionNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(DirectionNode left) {
+        this.left = left;
+    }
+
+    public DirectionNode getRight() {
+        return right;
+    }
+
+    public void setRight(DirectionNode right) {
+        this.right = right;
+    }
+}
