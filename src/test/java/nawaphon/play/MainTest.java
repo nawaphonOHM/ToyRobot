50304f2694b6516e02698ae04d46a64cbee6f0ad
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-16 00:30:08 +0700

    Add `originalSystemOut` field in `MainTest` for output stream handling

--- src/test/java/nawaphon/play/MainTest.java
+++ src/test/java/nawaphon/play/MainTest.java
@@ -3,12 +3,14 @@ package nawaphon.play;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.InputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     private final InputStream originalSystemIn = System.in;
+    private final PrintStream originalSystemOut = System.out;
 
     @BeforeEach
     void setUp() {
