aa8bf199d307d11f56685b1cda3acd8e7823366b
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-16 00:30:35 +0700

    Add `outputStreamCaptor` field in `MainTest` for capturing system output in tests

--- src/test/java/nawaphon/play/MainTest.java
+++ src/test/java/nawaphon/play/MainTest.java
@@ -2,6 +2,7 @@ package nawaphon.play;
 
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 
@@ -11,6 +12,8 @@ class MainTest {
 
     private final InputStream originalSystemIn = System.in;
     private final PrintStream originalSystemOut = System.out;
+
+    private ByteArrayOutputStream outputStreamCaptor;
 
     @BeforeEach
     void setUp() {
