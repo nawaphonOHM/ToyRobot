ae5aae774dd1b7c6e6f2108742841c168fd9faa6
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-15 22:55:57 +0700

    add InitializePositionIncorrectException class

---
+++ src/main/java/nawaphon/play/exceptions/InitializePositionIncorrectException.java
@@ -0,0 +1,8 @@
+package nawaphon.play.exceptions;
+
+
+public class InitializePositionIncorrectException extends RuntimeException {
+    public InitializePositionIncorrectException(String message) {
+        super(message);
+    }
+}
